4bca3a4c63895a14b63e2cf61be729af0c2207d8
Senjosei <zaaa.warudooo@gmail.com>
2025-05-04 14:57:42 +0700

    quick hack

--- src/qt_gui/main_window.cpp
+++ src/qt_gui/main_window.cpp
@@ -1043,7 +1043,7 @@ void MainWindow::InstallDragDropPkg(std::filesystem::path file, int pkgNum, int 
         std::filesystem::path game_install_dir = last_install_dir;
 
         QString pkgType = QString::fromStdString(pkg.GetPkgFlags());
-        bool use_game_update = pkgType.contains("PATCH") && Config::getSeparateUpdateEnabled();
+        bool use_game_update = pkgType.contains("PATCH") && false;
 
         // Default paths
         auto game_folder_path = game_install_dir / pkg.GetTitleID();
